c15854a86df722e70815403f43285194ef04925e
KusumaBrata-Dev <hikmat.kbp.Gmail.com>
2026-03-18 14:41:53 +0700

    feat: add new

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,18 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss(),],
+  plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('react-router')) return 'router';
+            if (id.includes('react')) return 'vendor';
+            if (id.includes('recharts')) return 'charts';
+          }
+        },
+      },
+    },
+  },
 })
